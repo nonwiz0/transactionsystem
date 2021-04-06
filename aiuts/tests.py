0e9b4e6929d9f39ad768112650ec9e7ef2d6dd43
chanrose <bdchanbroset@gmail.com>
2021-04-06 14:43:20 +0700

    Working on test.py

---
+++ aiuts/tests.py
@@ -0,0 +1,138 @@
+from django.test import TestCase
+from .models import Transaction, User
+from django.urls import reverse
+
+# Create your tests here.
+class UserRegistrationTest(TestCase):
+    def test_registration_with_all_information(self):
+        response = self.client.post(reverse('aiuts:create_acc'), {"fullname": "Testing01", "password": "Asd,car15"}, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "afe600a43cea6bdaf6c362905db6b883 has been created!")
+    
+    def test_registration_bypass_fullname(self):
+        response = self.client.post(reverse('aiuts:create_acc'), {"fullname": "", "password": "Asd,car15"}, follow=True)
+        self.assertEqual(response.status_code, 200)
+        blank_fullname = User.objects.filter(pk="d41d8cd98f00b204e9800998ecf8427e").exists()
+        self.assertEqual(False, blank_fullname)
+ 
+    def test_registration_bypass_password(self):
+        response = self.client.post(reverse('aiuts:create_acc'), {"fullname": "Testing01", "password": ""}, follow=True)
+        self.assertEqual(response.status_code, 200)
+        blank_password = User.objects.get(pk="afe600a43cea6bdaf6c362905db6b883").password == 'd41d8cd98f00b204e9800998ecf8427e'
+        self.assertEqual(False, blank_password)
+ 
+    def test_registration_bypass_blank_both(self):
+        response = self.client.post(reverse('aiuts:create_acc'), {"fullname": "", "password": ""}, follow=True)
+        self.assertEqual(response.status_code, 200)
+        blank_fullname = User.objects.filter(pk="d41d8cd98f00b204e9800998ecf8427e").exists()
+        blank_password = User.objects.get(pk="d41d8cd98f00b204e9800998ecf8427e").password == 'd41d8cd98f00b204e9800998ecf8427e'
+        blank_both = blank_fullname == blank_password
+        self.assertEqual(False, blank_both)
+
+
+class UserCheckBalanceTest(TestCase):
+    def test_check_balance_with_detail(self):
+        response = self.client.post(reverse('aiuts:create_acc'), {"fullname": "Testing01", "password": "Asd,car15"}, follow=True)
+        response = self.client.post(reverse('aiuts:checkbalance'), {"acc_id": "afe600a43cea6bdaf6c362905db6b883", "password":"Asd,car15"}, follow=True)
+        self.assertContains(response, "Your account has")
+
+    def test_check_balance_with_no_acc_id(self):
+        response = self.client.post(reverse('aiuts:create_acc'), {"fullname": "Testing01", "password": "Asd,car15"}, follow=True)
+        response = self.client.post(reverse('aiuts:checkbalance'), {"acc_id": "", "password":"Asd,car15"}, follow=True)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_check_balance_with_no_pass(self):
+        response = self.client.post(reverse('aiuts:create_acc'), {"fullname": "Testing01", "password": "Asd,car15"}, follow=True)
+        response = self.client.post(reverse('aiuts:checkbalance'), {"acc_id": "afe600a43cea6bdaf6c362905db6b883", "password":""}, follow=True)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_check_balance_with_no_id_pw(self):
+        response = self.client.post(reverse('aiuts:create_acc'), {"fullname": "Testing01", "password": "Asd,car15"}, follow=True)
+        response = self.client.post(reverse('aiuts:checkbalance'), {"acc_id": "", "password":""}, follow=True)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_check_balance_with_non_existing_id(self):    
+        response = self.client.post(reverse('aiuts:checkbalance'), {"acc_id": "adsfawefuihewifhui23123412", "password":"asdf2323"}, follow=True)
+        self.assertEqual(response.status_code, 200)
+    
+class UserSendMoneyTest(TestCase):
+    def test_send_money_acc_with_detail(self):
+        password = "Asd,car15"
+        self.client.post(reverse('aiuts:create_acc'), {"fullname": "Testing01", "password": password}, follow=True)
+        self.client.post(reverse('aiuts:create_acc'), {"fullname": "Testing02", "password": password}, follow=True)
+        acc_1_id = "afe600a43cea6bdaf6c362905db6b883"
+        acc_1_balance = User.objects.get(pk=acc_1_id).balance
+        acc_2_id = "1501b35612484ab78d6e6c5e52ba1328"
+        curr_acc_2_balance = User.objects.get(pk=acc_2_id).balance
+        response = self.client.post(reverse('aiuts:sendmoney'), {"source": acc_1_id, "destination": acc_2_id, "amount": acc_1_balance / 2, "password": password, "remark": "Test sending money from first account to the second acc" }, follow=True)
+        acc_2_get_more_money = User.objects.get(pk=acc_2_id).balance > curr_acc_2_balance
+        self.assertEqual(True, acc_2_get_more_money)
+    
+    def test_send_money_acc_with_detail(self):
+        password = "Asd,car15"
+        self.client.post(reverse('aiuts:create_acc'), {"fullname": "Testing01", "password": password}, follow=True)
+        acc_1_id = "afe600a43cea6bdaf6c362905db6b883"
+        curr_acc_1_balance = User.objects.get(pk=acc_1_id).balance
+        response = self.client.post(reverse('aiuts:sendmoney'), {"source": acc_1_id, "destination": acc_1_id, "amount": curr_acc_1_balance / 2, "password": password, "remark": "Test sending money from first account to the its own acc" }, follow=True)
+        acc_1_has_the_same_amount = User.objects.get(pk=acc_1_id).balance == curr_acc_1_balance
+        self.assertEqual(True, acc_1_has_the_same_amount)
+    
+    def test_send_money_acc_with_incorrect_pw(self):
+        password = "Asd,car15"
+        wrong_pass = "Aasdfasdf"
+        self.client.post(reverse('aiuts:create_acc'), {"fullname": "Testing01", "password": password}, follow=True)
+        self.client.post(reverse('aiuts:create_acc'), {"fullname": "Testing02", "password": password}, follow=True)
+        acc_1_id = "afe600a43cea6bdaf6c362905db6b883"
+        curr_acc_1_balance = User.objects.get(pk=acc_1_id).balance
+        acc_2_id = "1501b35612484ab78d6e6c5e52ba1328"
+        curr_acc_2_balance = User.objects.get(pk=acc_2_id).balance
+        response = self.client.post(reverse('aiuts:sendmoney'), {"source": acc_1_id, "destination": acc_2_id, "amount": curr_acc_1_balance / 2, "password": wrong_pass, "remark": "Test sending money from first account to the second acc" }, follow=True)
+        acc_2_get_more_money = User.objects.get(pk=acc_2_id).balance > curr_acc_2_balance
+        # Expected to be false because the password is incorrect
+        self.assertEqual(False, acc_2_get_more_money)
+    
+    def test_send_money_acc_with_nonexisting_recipient(self):
+        password = "Asd,car15"
+        self.client.post(reverse('aiuts:create_acc'), {"fullname": "Testing01", "password": password}, follow=True)
+        acc_1_id = "afe600a43cea6bdaf6c362905db6b883"
+        acc_1_balance = User.objects.get(pk=acc_1_id).balance
+        response = self.client.post(reverse('aiuts:sendmoney'), {"source": acc_1_id, "destination": "fasdfawefhaweiufhuiew", "amount": acc_1_balance / 2, "password": password, "remark": "Test sending money from first account to the second acc" }, follow=True)
+        acc_balance_remain_the_same = User.objects.get(pk=acc_1_id).balance == acc_1_balance
+        self.assertEqual(True, acc_balance_remain_the_same)
+    
+    def test_send_money_acc_with_incorrect_acc_id(self):
+        password = "Asd,car15"
+        self.client.post(reverse('aiuts:create_acc'), {"fullname": "Testing01", "password": password}, follow=True)
+        self.client.post(reverse('aiuts:create_acc'), {"fullname": "Testing02", "password": password}, follow=True)
+        acc_1_id = "afe600a43cea6bdaf6c362905db6b883"
+        acc_1_balance = User.objects.get(pk=acc_1_id).balance
+        acc_2_id = "1501b35612484ab78d6e6c5e52ba1328"
+        curr_acc_2_balance = User.objects.get(pk=acc_2_id).balance
+        response = self.client.post(reverse('aiuts:sendmoney'), {"source": "erhsifrjiuer", "destination": acc_2_id, "amount": acc_1_balance / 2, "password": password, "remark": "Test sending money from first account to the second acc" }, follow=True)
+        self.assertEqual(response.status_code, 200)
+
+class UserSummaryTransactionTest(TestCase):
+    def test_summary_transaction_with_detail(self):
+        password = "Asd,car15"
+        remark = "Test sending money from first account to the second acc"
+        self.client.post(reverse('aiuts:create_acc'), {"fullname": "Testing01", "password": password}, follow=True)
+        self.client.post(reverse('aiuts:create_acc'), {"fullname": "Testing02", "password": password}, follow=True)
+        acc_1_id = "afe600a43cea6bdaf6c362905db6b883"
+        acc_1_balance = User.objects.get(pk=acc_1_id).balance
+        acc_2_id = "1501b35612484ab78d6e6c5e52ba1328"
+        curr_acc_2_balance = User.objects.get(pk=acc_2_id).balance
+        self.client.post(reverse('aiuts:sendmoney'), {"source": acc_1_id, "destination": acc_2_id, "amount": acc_1_balance / 2, "password": password, "remark": remark }, follow=True)
+        acc_2_get_more_money = User.objects.get(pk=acc_2_id).balance > curr_acc_2_balance
+        response = self.client.post(reverse('aiuts:getsummaryoftransaction'), {"acc_id": acc_1_id, "password": password}, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, acc_2_id)
+        self.assertContains(response, remark)
+
+
+        
+    
+    
+
+    
+    
+        
---
+++ aiuts/views.py
@@ -0,0 +1,156 @@
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import get_object_or_404, render, redirect
+from django.urls import reverse
+from django.template import loader
+from django.views import generic
+from .models import User, Transaction
+from django.utils import timezone
+import hashlib
+from random import random
+from django.contrib import messages
+
+class IndexView(generic.ListView):
+    template_name = 'aiuts/index.html'
+    context_object_name = 'all_user'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return User.objects.all()[:]
+
+class LoginView(generic.TemplateView):
+    template_name = 'aiuts/login.html'
+
+class SignupView(generic.TemplateView):
+    template_name = 'aiuts/sign_up.html'
+
+class GetbalanceView(generic.TemplateView):
+    template_name = 'aiuts/get_balance.html'
+
+class GetaccidView(generic.TemplateView):
+    template_name = 'aiuts/get_acc_id.html'
+
+def create_acc(request):
+    fullname = request.POST['fullname']
+    password = request.POST['password']
+    acc_id = hashlib.md5(str.encode(fullname)).hexdigest()
+    hash_pw = hashlib.md5(str.encode(password)).hexdigest()
+    for acc in User.objects.all():
+        if acc.acc_id == acc_id:
+            messages.info(request, 'This name is already registered once!')
+            return HttpResponseRedirect(reverse('aiuts:signup'))
+    new_acc = User(acc_id, hash_pw, random() * 100)    
+    new_acc.save()
+    messages.info(request, '{} has been created!'.format(new_acc.acc_id))
+    return HttpResponseRedirect(reverse('aiuts:index'))
+
+def check_balance(request):
+    acc_id = request.POST['acc_id']
+    password = request.POST['password']
+    hash_pw = hashlib.md5(str.encode(password)).hexdigest()
+    for acc in User.objects.all():
+        if acc.acc_id == acc_id and acc.password == hash_pw:
+            messages.info(request, 'Your account has {:.2f} Baht'.format(acc.balance))
+            return HttpResponseRedirect(reverse('aiuts:getbalance'))
+            #return redirect(request.META['HTTP_REFERER'])
+        if acc.acc_id == acc_id and acc.password != hash_pw:
+            messages.info(request, 'Incorrect credential')
+           # return HttpReponseRedirect(reverse('aiuts:getbalance'))
+            return redirect(request.META['HTTP_REFERER'])
+
+
+def check_accid(request):
+    fullname = request.POST['fullname']
+    password = request.POST['password']
+    hash_acc = hashlib.md5(str.encode(fullname)).hexdigest()
+    hash_pw = hashlib.md5(str.encode(password)).hexdigest()
+    for acc in User.objects.all():
+        if acc.acc_id == hash_acc and acc.password == hash_pw:
+            messages.info(request, 'Acc ID: {}'.format(acc.acc_id))
+            return redirect(request.META['HTTP_REFERER'])
+        if acc.acc_id == hash_acc and acc.password != hash_pw:
+            messages.info(request, 'Incorrect credential')
+            return redirect(request.META['HTTP_REFERER'])
+
+def check_account(request):
+    acc_id = request.POST['acc_id']
+    password = request.POST['password']
+    hash_pw = hashlib.md5(str.encode(password)).hexdigest()
+    for acc in User.objects.all():
+        if acc.acc_id == acc_id and acc.password == hash_pw:
+            template = loader.get_template('aiuts/account_dash.html')
+            user = User.objects.get(pk=acc_id)
+            context = {'user': user}
+            messages.info(request, 'Login successfully')
+            return HttpResponse(template.render(context, request))
+    messages.info(request, 'Incorrect credential')
+    return HttpResponseRedirect(reverse('aiuts:login'))
+
+
+def send_money(request):
+    acc_id = request.POST['source']
+    recipient = request.POST['destination'].strip()
+    amount = float(request.POST['amount'])
+    password = request.POST['password']
+    hash_pw = hashlib.md5(str.encode(password)).hexdigest()
+    remark = request.POST['remark']
+    for acc in User.objects.all():
+        if acc.acc_id == acc_id and acc.password == hash_pw:
+            if amount < acc.balance and User.objects.filter(pk=recipient).exists():
+                recipient_acc = User.objects.get(pk=recipient)
+                acc.balance -= amount
+                recipient_acc.balance += amount
+                recipient_acc.save()
+                acc.save()
+                messages.info(request, "Money sent successfully, you have {} baht left".format(acc.balance))
+                transaction = Transaction(sender=acc, recipient=recipient_acc, amount=amount, remark=remark)
+                transaction.save()
+                return HttpResponseRedirect(reverse('aiuts:getbalance'))
+                # return redirect(request.META['HTTP_REFERER'])
+            else:
+                messages.info(request, "Failed to send, please check the address")
+                return HttpResponseRedirect(reverse('aiuts:getbalance'))
+        if acc.acc_id == acc_id and acc.password != hash_pw:
+            messages.info(request, 'Incorrect credential')
+            return HttpResponseRedirect(reverse('aiuts:getbalance'))
+           # return redirect(request.META['HTTP_REFERER'])
+    return HttpResponseRedirect(reverse('aiuts:getbalance'))
+
+def deposit_money(request):
+    acc_id = request.POST['acc_id']
+    amount = float(request.POST['amount'])
+    password = request.POST['password']
+    hash_pass = hashlib.md5(str.encode(password)).hexdigest()
+    if User.objects.filter(pk=acc_id).exists():
+        user_acc = User.objects.get(pk=acc_id)
+        if hash_pass == user_acc.password:
+            user_acc.balance += amount
+            user_acc.save()
+            record = Transaction(sender=user_acc, recipient=user_acc, amount=amount, remark="Deposit")
+            record.save()
+            messages.info(request, "Your balance right now is {:.2f}".format(user_acc.balance))
+            return redirect(request.META['HTTP_REFERER'])
+
+class TransactionView(generic.TemplateView):
+    template_name = 'aiuts/get_summary_with_acc.html'
+
+class SendMoneyView(generic.TemplateView):
+    template_name = 'aiuts/send_money.html'
+
+
+def get_summary_of_transaction(request):
+    acc_id = request.POST['acc_id']
+    password = request.POST['password']
+    hash_pass = hashlib.md5(str.encode(password)).hexdigest()
+    template = loader.get_template('aiuts/get_summary.html')
+    if User.objects.filter(pk=acc_id).exists():
+        user_acc = User.objects.get(pk=acc_id)
+        if hash_pass == user_acc.password:
+            all_transaction = list(Transaction.objects.filter(sender=acc_id))+list(Transaction.objects.filter(recipient=acc_id)[:])
+            context = {
+                'all_transaction': all_transaction,
+                'user': acc_id
+            }
+            return HttpResponse(template.render(context, request))
+    messages.info(request, "Incorrect password!")
+    return redirect(request.META['HTTP_REFERER'])
+
