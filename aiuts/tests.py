29728181baea7055dd8d45ad8b5160539520965b
Chanbroset <bdchanbroset@gmail.com>
2021-04-09 00:03:54 +0700

    update test's description

--- aiuts/tests.py
+++ aiuts/tests.py
@@ -5,17 +5,27 @@ from django.urls import reverse
 # Create your tests here.
 class UserRegistrationTest(TestCase):
     def test_registration_with_all_information(self):
+        """
+        This testing to see whether the signing up works when all the information is provided
+        """
         response = self.client.post(reverse('aiuts:create_acc'), {"fullname": "Testing01", "password": "Asd,car15"}, follow=True)
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "afe600a43cea6bdaf6c362905db6b883 has been created!")
     
     def test_registration_bypass_fullname(self):
+        """
+        This testing is checking the response of the backend when the required part of the fullname is bypass. To see whether the system will
+        accept with the blank full name or not.
+        """
         response = self.client.post(reverse('aiuts:create_acc'), {"fullname": "", "password": "Asd,car15"}, follow=True)
         self.assertEqual(response.status_code, 200)
         blank_fullname = User.objects.filter(pk="d41d8cd98f00b204e9800998ecf8427e").exists()
         self.assertEqual(False, blank_fullname)
  
     def test_registration_bypass_password(self):
+        """
+        Like the previous test, but this one is to check the password.
+        """
         response = self.client.post(reverse('aiuts:create_acc'), {"fullname": "Testing01", "password": ""}, follow=True)
         self.assertEqual(response.status_code, 200)
         if User.objects.filter(pk="afe600a43cea6bdaf6c362905db6b883").exists():
@@ -23,6 +33,9 @@ class UserRegistrationTest(TestCase):
             self.assertEqual(False, blank_password)
  
     def test_registration_bypass_blank_both(self):
+        """
+        This testing is the combination of both bypassing full name and password.
+        """
         response = self.client.post(reverse('aiuts:create_acc'), {"fullname": "", "password": ""}, follow=True)
         self.assertEqual(response.status_code, 200)
         blank_fullname = User.objects.filter(pk="d41d8cd98f00b204e9800998ecf8427e").exists()
@@ -35,6 +48,9 @@ class UserRegistrationTest(TestCase):
 
 class UserCheckBalanceTest(TestCase):
     def test_check_balance_with_detail(self):
+        """
+        This testing is to check the balance to see whether it can appear out properly or not. We proceed first by creating an account, then next, we can check the balance.
+        """
         response = self.client.post(reverse('aiuts:create_acc'), {"fullname": "Testing01", "password": "Asd,car15"}, follow=True)
         response = self.client.post(reverse('aiuts:checkbalance'), {"acc_id": "afe600a43cea6bdaf6c362905db6b883", "password":"Asd,car15"}, follow=True)
         self.assertContains(response, "Your account has")
@@ -73,6 +89,9 @@ class UserCheckBalanceTest(TestCase):
     
 class UserSendMoneyTest(TestCase):
     def test_send_money_acc_with_detail(self):
+        """
+        This test case to send money to another acc. We starts with creating two samples acc, one as a sender and another as a receiver. Then we can start sending and we confirm it by checking whether the receiver get more money than before the sending or not.
+        """
         password = "Asd,car15"
         self.client.post(reverse('aiuts:create_acc'), {"fullname": "Testing01", "password": password}, follow=True)
         self.client.post(reverse('aiuts:create_acc'), {"fullname": "Testing02", "password": password}, follow=True)
@@ -85,6 +104,9 @@ class UserSendMoneyTest(TestCase):
         self.assertEqual(True, acc_2_get_more_money)
     
     def test_send_money_acc_to_its_own_acc(self):
+        """
+        The test purpose here to prevent one acc sending money to its own acc that might lost the money in certain result.
+        """
         password = "Asd,car15"
         self.client.post(reverse('aiuts:create_acc'), {"fullname": "Testing01", "password": password}, follow=True)
         acc_1_id = "afe600a43cea6bdaf6c362905db6b883"
@@ -95,6 +117,9 @@ class UserSendMoneyTest(TestCase):
         self.assertEqual(True, acc_1_has_the_same_amount)
     
     def test_send_negative_amount_money_acc_with_detail(self):
+        """
+        This testing to counter the the ammount can be hacked to be negative in which can negate and make the sender money to increase instead. 
+        """
         password = "Asd,car15"
         self.client.post(reverse('aiuts:create_acc'), {"fullname": "Testing01", "password": password}, follow=True)
         acc_1_id = "afe600a43cea6bdaf6c362905db6b883"
@@ -104,6 +129,9 @@ class UserSendMoneyTest(TestCase):
         self.assertEqual(True, acc_1_has_the_same_amount)
    
     def test_send_insufficient_money_acc_with_detail(self):
+        """
+        This test to make sure that user have enough money first to send
+        """
         password = "Asd,car15"
         self.client.post(reverse('aiuts:create_acc'), {"fullname": "Testing01", "password": password}, follow=True)
         acc_1_id = "afe600a43cea6bdaf6c362905db6b883"
@@ -113,6 +141,9 @@ class UserSendMoneyTest(TestCase):
         self.assertEqual(True, acc_1_has_the_same_amount)
    
     def test_send_money_acc_with_incorrect_pw(self):
+        """
+        This test is to cancel users action when entered wrong pw
+        """
         password = "Asd,car15"
         wrong_pass = "Aasdfasdf"
         self.client.post(reverse('aiuts:create_acc'), {"fullname": "Testing01", "password": password}, follow=True)
@@ -127,6 +158,9 @@ class UserSendMoneyTest(TestCase):
         self.assertEqual(False, acc_2_get_more_money)
     
     def test_send_money_acc_with_nonexisting_recipient(self):
+        """
+        This test to find whether the money will gone to nonexisting recipient or not.
+        """
         password = "Asd,car15"
         self.client.post(reverse('aiuts:create_acc'), {"fullname": "Testing01", "password": password}, follow=True)
         acc_1_id = "afe600a43cea6bdaf6c362905db6b883"
@@ -136,6 +170,9 @@ class UserSendMoneyTest(TestCase):
         self.assertEqual(True, acc_balance_remain_the_same)
     
     def test_send_money_acc_with_incorrect_acc_id(self):
+        """
+        Checking whether the incorrect or different acc_id as sender will work
+        """
         password = "Asd,car15"
         self.client.post(reverse('aiuts:create_acc'), {"fullname": "Testing01", "password": password}, follow=True)
         self.client.post(reverse('aiuts:create_acc'), {"fullname": "Testing02", "password": password}, follow=True)
@@ -148,6 +185,9 @@ class UserSendMoneyTest(TestCase):
 
 class UserSummaryTransactionTest(TestCase):
     def test_summary_transaction_with_detail(self):
+        """
+        To see or check whether the summary is working well displaying the records
+        """
         password = "Asd,car15"
         remark = "Test sending money from first account to the second acc"
         self.client.post(reverse('aiuts:create_acc'), {"fullname": "Testing01", "password": password}, follow=True)
